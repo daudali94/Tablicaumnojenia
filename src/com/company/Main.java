8431b8be056cad8aea5d8adf31f20a096613758d
daudali94 <dnishanbai@gmail.com>
2022-01-31 16:51:46 +0600

    tablica commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int a=1; a<=10; a++)
+
+        {
+            System.out.println("5 * "+ a + " = "+ 5*a );
+        }
+    }
+}
